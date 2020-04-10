89509e53ba39e1a1b530c815585c3c733f6af40b
cbi11 <58070870+cbi11@users.noreply.github.com>
2020-04-10 01:41:32 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+document.getElementById('burger').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '100%';
+})
+document.getElementById('cls').addEventListener('click', function(){
+    document.getElementById('mobile_menu').style.width = '0'
+})
