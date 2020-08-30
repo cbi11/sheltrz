0e7662992cdb647565f19a29d2b3cc2601487ae5
cbi11 <58070870+cbi11@users.noreply.github.com>
2020-08-30 14:31:42 -0700

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('burger').addEventListener('click', function(){
-    document.getElementById('mobile_menu').style.width = '100%';
-})
-document.getElementById('cls').addEventListener('click', function(){
-    document.getElementById('mobile_menu').style.width = '0'
-})
